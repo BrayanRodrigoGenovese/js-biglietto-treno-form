95ca51cef08ad3202c208e36a7a7ab24cbad3f3b
Brayan <bittolo.8bit@gmail.com>
2026-01-13 17:06:24 +0100

    update parameter names in ticketPriceCalculator and adding JSDoc for clarity,

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+/**
+ * Calculates the ticket price based on age and distance, applying a discount if necessary
+ * @param {number} age - The age of the passenger.
+ * @param {number} km - The distance in kilometers.
+ * @param {number} price - The base price per kilometer
+ * @param {number} seniorPrice - For passengers above 65.
+ * @param {number} juniorPrice - For passengers under 18.
+ * @returns
+ */
 function ticketPriceCalculator(
   age,
   km,
   price = 0.21,
-  seniorDiscount = 0.6,
-  juniorDiscount = 0.8
+  seniorPrice = 0.6,
+  juniorPrice = 0.8
 ) {
-  const discount = age >= 65 ? seniorDiscount : age < 18 ? juniorDiscount : 1;
+  const discount = age >= 65 ? seniorPrice : age < 18 ? juniorPrice : 1;
   return price * km * discount;
 }
